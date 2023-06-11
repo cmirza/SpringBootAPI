b7ca4129fa48fcad905538186c7ebb8958ed791f
cmirza <mail@cmirza.com>
2023-06-10 17:44:27 -0700

    adding pagination

--- src/main/java/com/cmirza/springbootmongodbapi/controller/UserController.java
+++ src/main/java/com/cmirza/springbootmongodbapi/controller/UserController.java
@@ -2,11 +2,15 @@ package com.cmirza.springbootmongodbapi.controller;
 
 import com.cmirza.springbootmongodbapi.model.User;
 import com.cmirza.springbootmongodbapi.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,8 +22,13 @@ public class UserController {
         this.userRepository = userRepository;
     }
     @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public Page<User> getAllUsers(@RequestParam Map<String, String> params) {
+        int page = Integer.parseInt(params.getOrDefault("page", "0"));
+        int size = Integer.parseInt(params.getOrDefault("size", "5"));
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        return userRepository.findAll(pageable);
     }
 
     @PostMapping
